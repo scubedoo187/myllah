21ac8ffdbfabf48bb2cccc68e23788239373d207
Jung's Laptop <sublime718@hotmail.com>
2014-11-16 00:25:16 +0900

    11.16 GUI modi

--- myllah/myllah/src/com/capstonedesign/myllah/MainActivity.java
+++ myllah/myllah/src/com/capstonedesign/myllah/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        setTitle("메뉴 선택");
+        setTitle("메뉴 선택"); 
         
         startActivity(new Intent(this, LogoActivity.class));
         
